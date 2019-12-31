c4bef686013a30db2a0e06125574a68bcfd0f7b8
cyaxares <me@siamand.cc>
2019-12-31 03:47:00 +0100

    feat: project scoffolding

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {
+   
+}
+</script>
+
+<template>
+  <div id="app">
+      Light UI
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
