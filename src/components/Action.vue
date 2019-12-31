24d5ba61a3924a6c7cd2f56b10919f26864bdeeb
cyaxares <me@siamand.cc>
2019-12-31 15:04:52 +0100

    feat: action

--- src/components/Action.vue
+++ src/components/Action.vue
@@ -1,4 +1,3 @@
-
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
@@ -9,26 +8,31 @@ export default class Action extends Vue {
   @Prop()
   label!: string;
 
+  @Prop({ default: "button" })
+  style!: string;
+
   onClick() {
     this.$emit("click");
   }
 }
 </script>
 <template>
-  <button class="light-action" @click="onClick">
+  <button class="light-action" :class="style" @click="onClick">
     <slot name="prepend" />
-    <slot>{{label}}</slot>
+    <slot>{{ label }}</slot>
     <slot name="append" />
   </button>
 </template>
 <style lang="scss" scoped>
 .light {
   &-action {
-    border: solid thin #ccc;
     border-radius: 2px;
-    background: linear-gradient(to top, #ece9e6, #ffffff);
     padding: 3px;
     width: 100%;
+    &.button {
+      border: solid thin #ccc;
+      background: linear-gradient(to top, #ece9e6, #ffffff);
+    }
 
     &.on {
       background: linear-gradient(to top, #cccccc, #efefef);
